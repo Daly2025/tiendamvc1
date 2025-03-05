416edca774c98e5ceb5be8d275779266a156b6b0
Daly2025 <lydiafdez84@gmail.com>
2025-03-05 18:07:47 +0100

    css

--- app/views/admin/home.php
+++ app/views/admin/home.php
@@ -11,51 +11,89 @@
         integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="<?= base_url() ?>assets/css/style.css">
+    <style>
+        .menu ul {
+            list-style-type: none;
+            display: flex;
+            justify-content: space-around;
+            padding: 0;
+        }
+
+        .menu ul li {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            border: 2px solid;
+            width: 120px;
+            height: 120px;
+            margin: 10px;
+            padding: 10px;
+        }
+
+        .menu ul li:nth-child(1) {
+            border-color: red;
+        }
+
+        .menu ul li:nth-child(2) {
+            border-color: blue;
+        }
+
+        .menu ul li:nth-child(3) {
+            border-color: green;
+        }
+
+        .menu ul li:nth-child(4) {
+            border-color: orange;
+        }
+
+        .menu ul li i {
+            font-size: 4rem;
+        }
+
+        .menu ul li p {
+            margin: 0;
+        }
+    </style>
 </head>
 
 <body>
     <div class="container">
-        <h1>Admin Shop</h1>
+        <h1>tienda</h1>
         <div class="menu">
             <ul>
                 <a href="<?= base_url() ?>provider">
                     <li>
                         <div>
-                            <i class="fa-solid fa-3x fa-building"></i>
+                            <i class="fa-solid fa-4x fa-building" style="color: red;"></i>
                             <p>Providers</p>
                         </div>
-
                     </li>
                 </a>
                 <a href="<?= base_url() ?>customer">
                     <li>
                         <div>
-                            <i class="fa-solid fa-3x fa-circle-user"></i>
+                            <i class="fa-solid fa-4x fa-circle-user" style="color: blue;"></i>
                             <p>Customer</p>
                         </div>
-
                     </li>
                 </a>
                 <a href="<?= base_url() ?>product">
                     <li>
                         <div>
-                            <i class="fa-solid fa-3x fa-shop"></i>
+                            <i class="fa-solid fa-4x fa-shop" style="color: green;"></i>
                             <p>Products</p>
                         </div>
-
                     </li>
                 </a>
                 <a href="<?= base_url() ?>sale">
                     <li>
                         <div>
-                        <i class="fa-brands fa-3x fa-salesforce"></i></i>
+                            <i class="fa-solid fa-4x fa-salesforce" style="color: orange;"></i>
                             <p>Sales</p>
                         </div>
-
                     </li>
                 </a>
-
-
             </ul>
         </div>
     </div>
